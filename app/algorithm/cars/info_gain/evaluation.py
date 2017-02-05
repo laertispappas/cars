f7fbf2efa4114adadd4b3ccb9b953c6f4f159328
Laertis Pappas <laertis.pappas@gmail.com>
2017-02-05 15:37:55 +0200

    top10 weight all results

--- app/algorithm/cars/info_gain/evaluation.py
+++ app/algorithm/cars/info_gain/evaluation.py
@@ -36,11 +36,6 @@ def evaluateRecommender(testSet, trainSet, recommender, simMeasure=None, nNeighb
         recall = 0 if len(testSet[user].keys()) == 0 else float(hit) / (len(testSet[user].keys()))
         f1score = 0 if hit == 0 or precision + recall == 0 else float(2 * precision * recall / (precision + recall))
 
-        # result["users"][user]["Precision"] = precision
-        # result["users"][user]["Recall"] = recall
-        # result["users"][user]["F1-score"] = f1score
-        # result["users"][user]["Hit-rate"] = hit
-
         totalPrecision += precision
         totalRecall += recall
         totalF1score += f1score
@@ -112,7 +107,6 @@ def KFold(data, recommender, simMeasure=sim_pearson, nNeighbors=40, topN=10, nFo
             trainSet, testSet = KFoldSplit(data, fold, nFolds)
 
             evaluation = evaluateRecommender(testSet, trainSet, recommender, simMeasure=simMeasure, nNeighbors=nNeighbors, topN=topN, type=type)
-            # result[type][fold] = evaluation["users"] # ctx | 2d: { foldNum:{ userId: { metrics } } }
             totalPrecision += evaluation["Precision"]
             totalRecall += evaluation["Recall"]
             totalF1score += evaluation["F1-score"]
@@ -126,51 +120,174 @@ def KFold(data, recommender, simMeasure=sim_pearson, nNeighbors=40, topN=10, nFo
         result[type]["F1-score"] = totalF1score / nFolds
         result[type]["Hit-rate"] = float(totalHitrate) / nFolds
     print("Execution time: {}".format(datetime.now() - start_time))
-    # print result
-
-    # plot_metrics_bar_for_each_fold(result, nFolds, 'Precision')
-    # plot_metrics_bar_for_each_fold(result, nFolds, 'Recall')
-    # plot_metrics_bar_for_each_fold(result, nFolds, 'F1-score')
-    # plot_metrics_bar_for_each_fold(result, nFolds, 'Hit-rate')
-
-    # plot_avg_metrics_for_all_folds_per_user(result, nFolds, type='Precision')
-    # plot_avg_metrics_for_all_folds_per_user(result, nFolds, type='Recall')
-    # plot_avg_metrics_for_all_folds_per_user(result, nFolds, type='F1-score')
-    # plot_avg_metrics_for_all_folds_per_user(result, nFolds, type='Hit-rate')
 
     print result['2d']['F1-score']
     print result['ctx']['F1-score']
 
     # plot_results(result, type='F1-score')
-
-    filters = recommender.filters
-    context = str(filters[0][0])
-    condition = str(filters[0][1])
-
-    filename = "top10_filter_results_" + context + "__" + condition + ".json"
-    results_to_json(result, filename)
-
     return result
 
 
+feature_dict = {
+    'rating': 0, 'age': 1, 'sex': 2, 'city': 3,
+    'country': 4, 'time': 5, 'daytype': 6, 'season': 7,
+    'location': 8, 'weather': 9, 'social': 10,
+    'endEmo': 11, 'dominantEmo': 12, 'mood': 13,
+    'physical': 14, 'decision': 15, 'interaction': 16
+}
+
+ContextMappings = {
+    '5': 'time',
+    '6': 'daytype',
+    '7': 'season',
+    '8': 'location',
+    '9': 'weather',
+    '10': 'social',
+    '11': 'endEmo',
+    '12': 'dominantEmo',
+    '13': 'mood',
+    '14': 'physical',
+    '15': 'decision',
+    '16': 'intercation'
+}
+
+ContextConditionMappings = {
+    '5': ['','Morning', 'Afternoon', 'Evening', 'Night'],
+    '6': ['','Working day', 'Weekend', 'Holiday'],
+    '7': ['','Spring', 'Summer', 'Autumn', 'Winter'],
+    '8': ['','Home', 'Public place', "Friend's house"],
+    '9': ['','Sunny / clear', 'Rainy', 'Stormy', 'Snowy', 'Cloudy'],
+    '10': ['','Alone', 'My partner', 'Friends', 'Colleagues', 'Parents', 'Public', 'My family'],
+    '11': ['','Sad', 'Happy', 'Scared', 'Surprised', 'Angry', 'Disgusted', 'Neutral'],
+    '12': ['', 'Sad', 'Happy', 'Scared', 'Surprised', 'Angry', 'Disgusted', 'Neutral'],
+    '13': ['', 'Positive', 'Neutral', 'Negative'],
+    '14': ['', 'Healthy', 'Ill'],
+    '15': ['', 'User decided which movie to watch', 'User was given a movie'],
+    '16': ['', 'first interaction with a movie', 'n-th interaction with a movie']
+}
+
+def plot_results(data, type=None):
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    data = {"ctx": {"10": {
+        "2d": {"Recall": 0.005904833772480831, "F1-score": 0.002873184912091745, "Precision": 0.0028925619834710746,
+               "Hit-rate": 0.028925619834710745},
+        "1": {"Recall": 0.0075215665530169815, "F1-score": 0.004049164938371134, "Precision": 0.004132231404958677,
+              "Hit-rate": 0.04132231404958678},
+        "3": {"Recall": 0.006568942391134904, "F1-score": 0.003340520606700167, "Precision": 0.0033057851239669425,
+              "Hit-rate": 0.03305785123966942},
+        "2": {"Recall": 0.008022721192800755, "F1-score": 0.004303309889219255, "Precision": 0.003925619834710743,
+              "Hit-rate": 0.03925619834710744},
+        "4": {"Recall": 0.005904833772480831, "F1-score": 0.002873184912091745, "Precision": 0.0028925619834710746,
+              "Hit-rate": 0.028925619834710745}}, "5": {
+        "2d": {"Recall": 0.005904833772480831, "F1-score": 0.002873184912091745, "Precision": 0.0028925619834710746,
+               "Hit-rate": 0.028925619834710745},
+        "1": {"Recall": 0.005494092661498426, "F1-score": 0.002425925155157705, "Precision": 0.0022727272727272726,
+              "Hit-rate": 0.022727272727272728},
+        "3": {"Recall": 0.007545435986126311, "F1-score": 0.004499230742626802, "Precision": 0.004545454545454546,
+              "Hit-rate": 0.045454545454545456},
+        "2": {"Recall": 0.005108304934634991, "F1-score": 0.0034822557652176666, "Precision": 0.00371900826446281,
+              "Hit-rate": 0.0371900826446281},
+        "4": {"Recall": 0.011615292441642385, "F1-score": 0.006202906649000282, "Precision": 0.005785123966942149,
+              "Hit-rate": 0.05785123966942149}}, "7": {
+        "2d": {"Recall": 0.005904833772480831, "F1-score": 0.002873184912091745, "Precision": 0.0028925619834710746,
+               "Hit-rate": 0.028925619834710745},
+        "1": {"Recall": 0.006354050331323058, "F1-score": 0.0030444094634815857, "Precision": 0.0026859504132231405,
+              "Hit-rate": 0.026859504132231406},
+        "3": {"Recall": 0.010362580107544471, "F1-score": 0.006555264043302914, "Precision": 0.006611570247933883,
+              "Hit-rate": 0.06611570247933884},
+        "2": {"Recall": 0.005732657463940886, "F1-score": 0.0027651527184980543, "Precision": 0.0028925619834710746,
+              "Hit-rate": 0.028925619834710745},
+        "4": {"Recall": 0.006441368316368316, "F1-score": 0.0032295778304681348, "Precision": 0.002892561983471074,
+              "Hit-rate": 0.028925619834710745}}, "6": {
+        "2d": {"Recall": 0.005904833772480831, "F1-score": 0.002873184912091745, "Precision": 0.0028925619834710746,
+               "Hit-rate": 0.028925619834710745},
+        "1": {"Recall": 0.005617496387028146, "F1-score": 0.0026098564402071236, "Precision": 0.0026859504132231405,
+              "Hit-rate": 0.026859504132231406},
+        "3": {"Recall": 0.005878345109628532, "F1-score": 0.0028427319595608702, "Precision": 0.0028925619834710746,
+              "Hit-rate": 0.028925619834710745},
+        "2": {"Recall": 0.006854982108992803, "F1-score": 0.003644264693866714, "Precision": 0.00371900826446281,
+              "Hit-rate": 0.0371900826446281}}}}
+
+    for context in data['ctx'].keys():
+        labels = []
+        n_groups = len(data['ctx'][context].keys())
+        baseline_metrics = (data['ctx'][context]['2d']['Precision'], data['ctx'][context]['2d']['Recall'], data['ctx'][context]['2d']['F1-score'])
+        contextual_metrics = AutoVivification()
+
+        labels.extend(['Baseline', 'Baseline', 'Baseline'])
+
+        for condition in data['ctx'][context].keys():
+            if condition == '2d':
+                continue
+            contextual_metrics[condition] = (data['ctx'][context][condition]['Precision'],
+                                             data['ctx'][context][condition]['Recall'],
+                                             data['ctx'][context][condition]['F1-score'])
+
+            label_name = ContextConditionMappings[context][int(condition)]
+            labels.extend([label_name, label_name, label_name])
+        print baseline_metrics
+        print contextual_metrics
+
+        fig, ax = plt.subplots()
+        index = np.arange(3)
+        bar_width = 0.15
+        opacity = 0.8
+
+        rects1 = plt.bar(index, baseline_metrics, bar_width,
+                         alpha=opacity,
+                         color='b',
+                         label='Baseline')
+        colors = {
+            '1': 'r',
+            '2': 'g',
+            '3': 'y',
+            '4': 'c',
+            '5': 'm',
+            '6': 'k',
+            '7': '#ee1f3f',
+            '8': '#123ef2'
+        }
+        i = 1
+        for condition in contextual_metrics.keys():
+            plt.bar(index + bar_width * i, contextual_metrics[condition], bar_width,
+                    alpha=opacity,
+                    color=colors[condition],
+                    label=ContextConditionMappings[context][int(condition)])
+            i += 1
+
+        rects = ax.patches
+        # Now make some labels
+        # labels = ["label%d" % i for i in xrange(len(rects))]
+        for rect, label in zip(rects, labels):
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width() / 2, height, label, ha='center', va='bottom')
+
+        # plt.xlabel('Metric')
+        # plt.ylabel('Value')
+        plt.title('Evaluation Metrics for ' + ContextMappings[context] + ' context')
+        plt.xticks(index + bar_width + 0.15, ('Precision', 'Recall', 'F1Score'))
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
 def evaluate():
+    plot_results([])
+    exit()
     from app.algorithm.cars.info_gain.info_gain_recommender import InfoGainRecommender
-    # feature_dict = {
-    #     'time': 5,
-    #     'daytype': 6,
-    #     'location': 8,
-    #     'social': 10,
-    #     'endEmo': 11,
-    #     'dominantEmo': 12,
-    #     'mood': 13
-    # }
     context_conditions = {
-        '10': range(1, 5),
         '5': range(1, 5),
         '6': range(1, 4),
         '7': range(1, 5),
         '8': range(1, 4),
+        '10': range(1, 5),
+        '11': range(1,8),
+        '12': range(1,8),
+        '13': range(1,4),
+        '15': range(1,3),
+        '16': range(1,3),
     }
+    result = AutoVivification()
     for context in context_conditions.keys():
         for condition in context_conditions[context]:
             print context
@@ -179,216 +296,15 @@ def evaluate():
             recommender = InfoGainRecommender(data_object)
             recommender.run()
             recommender.filters = [(int(context), condition)]
-            KFold(recommender.training_data, recommender)
+            current_results = KFold(recommender.training_data, recommender)
+            result['ctx'][str(context)]['2d'] = current_results['2d']
+            result['ctx'][str(context)][str(condition)] = current_results['ctx']
 
-
-def generate_next_context():
-    context_conditions = {
-        '10': range(1, 5),
-        '5': range(1, 5),
-        '6': range(1, 4),
-        '7': range(1, 5),
-        '8': range(1, 4),
-    }
-
-    def generate_pairs(context_conditions):
-        pairs = []
-        for context in context_conditions.keys():
-            conditions = context_conditions[context]
-            pairs.append((context, conditions))
-        return pairs
-
-    # [(context, [conditions]), (ctx2, [cond2])]
-    pairs = generate_pairs(context_conditions)
-
-    def __f(pairs):
-        pass
-
-
-def plot_results(data, type=None):
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    # data to plot
-    n_groups = 4
-    baseline = (90, 55, 40, 65)
-    context1 = (85, 62, 54, 20)
-    context2 = (100, 105, 154, 120)
-
-    # create plot
-    fig, ax = plt.subplots()
-    index = np.arange(n_groups)
-    bar_width = 0.25
-    opacity = 0.8
-
-    rects1 = plt.bar(index, baseline, bar_width,
-                     alpha=opacity,
-                     color='b',
-                     label='Frank')
-
-    rects2 = plt.bar(index + bar_width, context1, bar_width,
-                     alpha=opacity,
-                     color='g',
-                     label='Guido')
-
-    rects3 = plt.bar(index + bar_width + bar_width, context2, bar_width,
-                     alpha=opacity,
-                     color='r',
-                     label='Me')
-
-    plt.xlabel('Precision')
-    plt.ylabel('Context')
-    plt.title('Scores by person')
-    plt.xticks(index + bar_width, ('A', 'B', 'C', 'D'))
-    plt.legend()
-
-    plt.tight_layout()
-    plt.show()
-    exit()
+    filename = "top10_weight_all_results.json"
+    results_to_json(result, filename)
+    plot_results(result)
 
 def results_to_json(data, filename):
     import json
     with open(filename, 'w') as fp:
         json.dump(data, fp)
-
-def plot_avg_metrics_for_all_folds_per_user(data, nFolds, type='Precision'):
-    users_ids = data['2d'][0].keys()
-    users_ids.sort()
-
-    precisions = AutoVivification()
-    ctx_precisions = AutoVivification()
-
-    for fold in range(nFolds):
-        for user in users_ids:
-            precisions.setdefault(user, 0)
-            ctx_precisions.setdefault(user, 0)
-            precisions[user] += data['2d'][fold][user][type]
-            ctx_precisions[user] += data['ctx'][fold][user][type]
-
-
-    N = len(precisions)
-    ind = np.arange(N)  # the x locations for total precision bars
-    width = 0.35  # the width of the bars
-
-    fig, ax = plt.subplots()
-    avg_precision_per_user = []
-    avg__ctx_precision_per_user = []
-
-    for user in users_ids:
-        avg_precision_per_user.append(precisions[user])
-        avg__ctx_precision_per_user.append(ctx_precisions[user])
-
-    rects1 = ax.bar(ind, avg_precision_per_user, width, color='r')
-    rects2 = ax.bar(ind + width, avg__ctx_precision_per_user, width, color='y')
-
-    # add some text for labels, title and axes ticks
-    ax.set_ylabel(type)
-    ax.set_xlabel('users')
-    ax.set_xticks(ind + width)
-    ax.set_xticklabels(users_ids)
-
-    ax.legend((rects1[0], rects2[0]), ('Simple Recommendation ' + '(avg)', 'Contextual Recommendation' + '(avg)'))
-    plt.show()
-
-
-def plot_metrics_bar_for_each_fold(data, nFolds, type='Precision'):
-    users_ids = data['2d'][0].keys()
-    users_ids.sort()
-
-    for fold in range(nFolds):
-        precisions = []
-        ctx_precisions = []
-
-        for user in users_ids:
-            precisions.append(data['2d'][fold][user][type])
-            ctx_precisions.append(data['ctx'][fold][user][type])
-        N = len(precisions)
-        ind = np.arange(N)  # the x locations for total precision bars
-        width = 0.35  # the width of the bars
-
-        fig, ax = plt.subplots()
-        rects1 = ax.bar(ind, precisions, width, color='r')
-
-        rects2 = ax.bar(ind + width, ctx_precisions, width, color='y')
-
-        # add some text for labels, title and axes ticks
-        ax.set_ylabel(type)
-        ax.set_xlabel('users')
-        ax.set_xticks(ind + width)
-        ax.set_xticklabels(users_ids)
-
-        ax.legend((rects1[0], rects2[0]), ('Simple Recommendation ' + '(fold=' + str(fold) + ')' , 'Contextual Recommendation' + '(fold=' + str(fold) + ')'))
-        plt.show()
-
-
-def precision(user, recommendations, udb, all_udb):
-    """
-    recommendations: generated by get_recommendations
-    udb: test udb
-
-    precision = #tp / #tp + #fp
-    where:
-        #tp => Recommended and Used
-        #fp => Recommended and not Used
-    """
-    all = len(recommendations) + 0.00000000000000000001 # === tp + fp
-    tp = 0
-    fp = 0
-    for (rec_rating, rec_movie) in recommendations:
-        for movie in udb[user].keys():
-            if movie == rec_movie and udb[user][movie][RATING] >= OPTIMUM:
-                tp += 1
-                break
-        if rec_movie not in udb[user].keys():
-            fp += 1
-    return tp/float(tp + fp + 0.00000000000000000001)
-
-def recall(user, recommendations, udb):
-    """
-    :param user: Target user
-    :param recommendations: recommendation list generated for target user
-    :param udb: test dataset
-    :return:
-
-
-    recall = #tp / #tp + #fn
-    where:
-        #tp => Recommended and Used
-        #fn => Not recommended but Used
-    """
-    tp = 0
-    fn = 0
-    good_movies = 0
-
-    for movie in udb[user].keys():
-        if udb[user][movie][RATING] >= OPTIMUM:
-            good_movies += 1
-            found = False
-            for (x, y) in recommendations:
-                if movie == y:
-                    tp += 1
-                    found = True
-                    break
-            if not found:
-                fn += 1
-    return tp/float(tp + fn)
-
-def f1score(precision, recall):
-    if precision + recall == 0:
-        return 0
-    return 2 * (precision * recall)/(precision + recall)
-
-
-# Given udb datastore return 1/3 as a test dataset and 2/3 as train dataset
-def __remove_for_testing(self, udb, user):
-    limit = int(len(udb[user]) / 3.0)
-    test_udb = AutoVivification()
-    import copy;
-    train_udb, i = copy.deepcopy(udb), 0
-    for movie in udb[user]:
-        test_udb[user][movie] = train_udb[user][movie]
-        del (train_udb[user][movie])
-        i += 1
-        if i > limit: break
-    # print len(train_udb[user]), len(test_udb[user]), len(udb[user])
-    return train_udb, test_udb
